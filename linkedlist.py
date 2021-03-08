cc421dae371c254ab1db734fa2ebb0440083b7f2
shocho0101 <shocho0101@hotmail.co.jp>
2021-03-08 21:23:24 +0900

    C2

--- linkedlist.py
+++ linkedlist.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 class ListNode:
-    def __init__(self, data=0, next_node=None):
+    def __init__(self, data=0, next_node=None, prev_node=None):
         self.data: int = data
         self.next: Optional[ListNode] = next_node
+        self.prev: Optional[ListNode] = prev_node
